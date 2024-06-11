f6c5ebbcab3ed83a21c1212eae49748d0464c619
DESKTOP-5QT8APO\Vasyl Khreptak <VasylKhreptak12@gmail.com>
2024-06-11 19:58:15 +0300

    optimization: removed colliders from garage; feat: added collider remover tool;

---
+++ Assets/Scripts/Utilities/ColliderRemover.cs
@@ -0,0 +1,17 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Utilities
+{
+    public class ColliderRemover : MonoBehaviour
+    {
+        [Button]
+        private void Remove()
+        {
+            foreach (Collider collider in GetComponentsInChildren<Collider>(true))
+                DestroyImmediate(collider);
+
+            DestroyImmediate(this);
+        }
+    }
+}
