2425c1636c1f9dc9c1ff4e3b842199e99f41d8a9
DESKTOP-5QT8APO\Vasyl Khreptak <VasylKhreptak12@gmail.com>
2024-06-11 17:46:10 +0300

    feat: added simple Play Button; fix: fixed pressed button states;

---
+++ Assets/Scripts/UI/Buttons/PlayButton.cs
@@ -0,0 +1,32 @@
+using Infrastructure.Services.StaticData.Core;
+using Infrastructure.StateMachine.Game.States;
+using Infrastructure.StateMachine.Game.States.Core;
+using Infrastructure.StateMachine.Main.Core;
+using UI.Buttons.Core;
+using Zenject;
+
+namespace UI.Buttons
+{
+    public class PlayButton : BaseButton
+    {
+        private IStateMachine<IGameState> _stateMachine;
+        private IStaticDataService _staticDataService;
+
+        [Inject]
+        private void Constructor(IStateMachine<IGameState> stateMachine, IStaticDataService staticDataService)
+        {
+            _stateMachine = stateMachine;
+            _staticDataService = staticDataService;
+        }
+
+        protected override void OnClicked()
+        {
+            LoadSceneAsyncState.Payload payload = new LoadSceneAsyncState.Payload
+            {
+                SceneName = _staticDataService.Config.GameplayScene.Name,
+            };
+
+            _stateMachine.Enter<LoadSceneWithTransitionAsyncState, LoadSceneAsyncState.Payload>(payload);
+        }
+    }
+}
