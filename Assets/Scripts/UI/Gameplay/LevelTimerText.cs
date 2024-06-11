3699ce1fe248505f90bab3d494273d20091af4e1
DESKTOP-5QT8APO\Vasyl Khreptak <VasylKhreptak12@gmail.com>
2024-06-11 18:37:19 +0300

    feat: added LevelTimerText;

---
+++ Assets/Scripts/UI/Gameplay/LevelTimerText.cs
@@ -0,0 +1,26 @@
+using System;
+using Gameplay.TimeManagement;
+using UI.Texts.Core;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace UI.Gameplay
+{
+    public class LevelTimerText : ReactiveText
+    {
+        private LevelTimer _levelTimer;
+
+        [Inject]
+        private void Constructor(LevelTimer levelTimer) => _levelTimer = levelTimer;
+
+        protected override IObservable<string> GetObservable() => _levelTimer.RemainingTime.Select(ToTimeString);
+
+        private string ToTimeString(float totalSeconds)
+        {
+            int minutes = Mathf.FloorToInt(totalSeconds / 60f);
+            int seconds = Mathf.FloorToInt(totalSeconds % 60f);
+            return $"{minutes:00}:{seconds:00}";
+        }
+    }
+}
