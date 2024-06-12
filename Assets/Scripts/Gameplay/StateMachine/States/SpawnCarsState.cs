7b7dcd2614e2daa039696b06ffb4df56f19c02f7
DESKTOP-5QT8APO\Vasyl Khreptak <VasylKhreptak12@gmail.com>
2024-06-12 18:06:46 +0300

    feat: added car spawn via spawn points;

--- Assets/Scripts/Gameplay/StateMachine/States/SpawnCarsState.cs
+++ Assets/Scripts/Gameplay/StateMachine/States/SpawnCarsState.cs
@@ -1,7 +1,12 @@
 using Gameplay.StateMachine.States.Core;
+using Infrastructure.Data.Static;
+using Infrastructure.Data.Static.Core;
 using Infrastructure.Services.Log.Core;
+using Infrastructure.Services.StaticData.Core;
 using Infrastructure.StateMachine.Main.Core;
 using Infrastructure.StateMachine.Main.States.Core;
+using UnityEngine;
+using Zenject;
 
 namespace Gameplay.StateMachine.States
 {
@@ -9,16 +14,28 @@ namespace Gameplay.StateMachine.States
     {
         private readonly IStateMachine<IGameplayState> _stateMachine;
         private readonly ILogService _logService;
+        private readonly Prefabs _prefabs;
+        private readonly IInstantiator _instantiator;
+        private readonly CarSpawnPoints _carSpawnPoints;
 
-        public SpawnCarsState(IStateMachine<IGameplayState> stateMachine, ILogService logService)
+        public SpawnCarsState(IStateMachine<IGameplayState> stateMachine, ILogService logService, IStaticDataService staticDataService,
+            IInstantiator instantiator, CarSpawnPoints carSpawnPoints)
         {
             _stateMachine = stateMachine;
             _logService = logService;
+            _instantiator = instantiator;
+            _carSpawnPoints = carSpawnPoints;
+            _prefabs = staticDataService.Prefabs;
         }
 
         public void Enter()
         {
             _logService.Log("SpawnCarsState");
+
+            Transform spawnPoint = _carSpawnPoints[0];
+            Transform car = _instantiator.InstantiatePrefab(_prefabs[Prefab.BaseCar], spawnPoint.position, spawnPoint.rotation, null).transform;
+            CarCamera carCamera = _instantiator.InstantiatePrefabForComponent<CarCamera>(_prefabs[Prefab.CarCamera]);
+            carCamera.SetTarget(car);
 
             _stateMachine.Enter<WarmUpState>();
         }
